d7663f416fbdddd0407fa879530d65e9ea527774
Easyho._.0 <44223030+leejihoindaeyo@users.noreply.github.com>
2023-09-27 11:29:21 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+    int a, b;
+    cin >> a;
+    
+    b = a/4;
+    for(int i = 0; i < b; i++){
+        cout << "long ";
+    }
+    cout << "int" << endl;
+}
